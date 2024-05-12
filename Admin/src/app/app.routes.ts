a33134b5d54434f72b31ddf667dddea0706ffecc
akshaybhagwat76 <omkardhapte99@gmail.com>
2024-05-12 17:18:44 +0530

    Added Values

---
+++ Admin/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { CommanLayoutComponent } from './comman-layout/comman-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FoodsComponent } from './foods/foods.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+export const routes: Routes = [
+    {path:'',pathMatch:'full',redirectTo: 'login',},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {
+        path:'mains',
+        component:CommanLayoutComponent,
+        children:[
+            { path :'dashboard',component:DashboardComponent},
+            { path:'foods',component:FoodsComponent},
+            // { path :''}
+        ]
+    }
+];
---
+++ Admin/src/app/foods/foods.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foods',
+  standalone: true,
+  imports: [],
+  templateUrl: './foods.component.html',
+  styleUrl: './foods.component.css'
+})
+export class FoodsComponent {
+
+}
